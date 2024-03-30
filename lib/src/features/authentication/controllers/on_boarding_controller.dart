b2aaf8786528975ea2e77655e8beba18dba2833a
brianwereh <arekbrian6@gmail.com>
2024-03-30 19:43:14 +0300

    3rd boarding screen

--- lib/src/features/authentication/controllers/on_boarding_controller.dart
+++ lib/src/features/authentication/controllers/on_boarding_controller.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
+import 'package:trustify/src/constants/colors.dart';
+import 'package:trustify/src/constants/image_strings.dart';
+import 'package:trustify/src/constants/text_strings.dart';
 import 'package:trustify/src/features/authentication/models/model_on_boarding.dart';
 
 import '../screens/on_boarding/on_boarding_page_widget.dart';
