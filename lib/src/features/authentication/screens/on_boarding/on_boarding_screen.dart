2d2d47444464d7d843044984ff1adc763277a48a
brianwereh <arekbrian6@gmail.com>
2024-03-30 19:40:39 +0300

    2nd boarding screen

--- lib/src/features/authentication/screens/on_boarding/on_boarding_screen.dart
+++ lib/src/features/authentication/screens/on_boarding/on_boarding_screen.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:trustify/src/constants/colors.dart';
-import 'package:trustify/src/constants/image_strings.dart';
-import 'package:trustify/src/constants/sizes.dart';
-import 'package:trustify/src/constants/text_strings.dart';
-import 'package:trustify/src/features/authentication/models/model_on_boarding.dart';
-import 'package:trustify/src/features/authentication/screens/on_boarding/on_boarding_page_widget.dart';
+
+import '../../controllers/on_boarding_controller.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({Key? key}) : super(key: key);
@@ -31,10 +29,9 @@ class OnBoardingScreen extends StatelessWidget {
             child: OutlinedButton(
               onPressed: () => obController.animateToNextSlide(),
               style: ElevatedButton.styleFrom(
-                side: const BorderSide(color: Colors.black26),
+                foregroundColor: Colors.white, side: const BorderSide(color: Colors.black26),
                 shape: const CircleBorder(),
                 padding: const EdgeInsets.all(20),
-                onPrimary: Colors.white,
               ),
               child: Container(
                 padding: const EdgeInsets.all(20.0),
